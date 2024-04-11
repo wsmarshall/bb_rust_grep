a67d098638e56f7f24d1472787a871cd78acf919
Warren <42559252+wsmarshall@users.noreply.github.com>
2024-04-11 10:23:30 -0700

    adds error checking for number of arguments

--- src/main.rs
+++ src/main.rs
@@ -22,6 +22,9 @@ struct Config {
 
 impl Config {
     fn new(args: &[String]) -> Config {
+        if args.let() < 3 {
+            panic!("Insufficient number of arguments");
+        }
         let query = args[1].clone();
         let file_path = args[2].clone();
 
