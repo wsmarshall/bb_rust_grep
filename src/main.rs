c461a47b4da34dd3b603157d77201f4d3bfa1da9
Warren <42559252+wsmarshall@users.noreply.github.com>
2024-04-11 09:47:05 -0700

    bugfixes config instantiation

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = parse_config(&args);
+    let config = Config::new(&args);
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
@@ -17,13 +17,6 @@ fn main() {
 struct Config {
     query: String,
     file_path: String,
-}
-
-fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let file_path = args[2].clone();
-
-    Config { query, file_path }
 }
 
 impl Config {
