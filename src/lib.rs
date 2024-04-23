d201a9e94e6d79d1d1a78cfff33a4b6faee40be7
Warren <42559252+wsmarshall@users.noreply.github.com>
2024-04-23 12:08:06 -0700

    refactors search to use iterators

--- src/lib.rs
+++ src/lib.rs
@@ -81,26 +81,16 @@ Trust me.";
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-
-    results
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let query = query.to_lowercase();
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query) {
-            results.push(line);
-        }
-    }
-
-    results
+    contents
+        .lines()
+        .to_lowercase()
+        .filter(|line| line.contains(query))
+        .collect()
 }
